b0ed36fc046c9d010ed073ce3f1c188f4437882f
Romain Moreau <romainmoreau@gmail.com>
2023-09-21 16:11:57 +0200

    Example of synchronous exception when the executor throws an exception

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import java.util.concurrent.Executor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements AsyncConfigurer {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+
+	@Override
+	public Executor getAsyncExecutor() {
+		return (command) -> {
+			throw new IllegalStateException("No thread available");
+		};
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		var demoService = SpringApplication.run(DemoApplication.class, args).getBean(DemoService.class);
+		try {
+			demoService.someAsyncTask();
+		} catch (IllegalStateException e) {
+			LOGGER.error("Synchronous exception example", e);
+		}
 	}
 }
