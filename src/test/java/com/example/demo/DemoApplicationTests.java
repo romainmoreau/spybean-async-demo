72d512fe71a46a3cf2bb930069ce4b318679f062
Romain Moreau <romainmoreau@gmail.com>
2023-09-21 16:49:12 +0200

    Refactoring to Spring only

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,29 +1,59 @@
 package com.example.demo;
 
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-@SpringBootTest
+import com.example.demo.DemoApplicationTests.ServiceConfiguration;
+
+@SpringJUnitConfig(classes = ServiceConfiguration.class)
 class DemoApplicationTests {
-	@SpyBean
+
+	@Autowired
 	private DemoService demoService;
 
 	@Test
 	void whenThenThrowTest() throws InterruptedException, ExecutionException {
 		when(demoService.someAsyncTask()).thenThrow(RuntimeException.class);
-		Assertions.assertThrows(RuntimeException.class, () -> demoService.someAsyncTask().get());
+		Assertions.assertThrows(RuntimeException.class, () -> demoService.someAsyncTask());
 	}
 
 	@Test
 	void doThrowWhenTest() throws InterruptedException, ExecutionException {
 		Mockito.doThrow(RuntimeException.class).when(demoService).someAsyncTask();
-		Assertions.assertThrows(RuntimeException.class, () -> demoService.someAsyncTask().get());
+		Assertions.assertThrows(RuntimeException.class, () -> demoService.someAsyncTask());
 	}
+
+	@EnableAsync
+	@Configuration
+	static class ServiceConfiguration {
+
+		@Bean
+		DemoService demoService() {
+			return spy(DemoService.class);
+		};
+
+	}
+
+	static class DemoService {
+
+		@Async
+		public CompletableFuture<Void> someAsyncTask() {
+			return CompletableFuture.completedFuture(null);
+		}
+
+	}
+
 }
